fce47c7946c3fcd3ed5589798d99c5b436de9271
Yipeng Sun <syp@umd.edu>
2022-01-12 23:04:00 +0100

    Set root password to empty for all test machines.

--- users/root/default.nix
+++ users/root/default.nix
@@ -1,14 +1,17 @@
 { self, lib, config, ... }:
+let
+  testHosts = [ "NixOS" "Thomas" ];
+in
 lib.mkMerge [{
   # main config
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC1s0sy5xORVQZcM7Yg1UcxqxGOazY41kci43OV0aqX7owjrxJKhezeOU0uehcvr2uaJykF5wRphaMjiY5tmaVyh35RKZ7tu5B7bx0FOjgATrUFAcBgKqzVMeCSmvmSUNK02HYrP+SOWbdgYECkyF+7PVxZoUefPnpBfGiqunfBWD5YrJMJPToFRqRW7Lcl+/6wIZQOAvPq8lvhfG89r9SvdiEX8umpYJKRgIl9k5wOsimTFJ5wLfq39sjECIzGCcbVLkiPzkOPLWRRgamICbiN4f0HF8kqdDU0mD1WZ5wHM72P68WKpHhMn9l+NEsGYik0fkW+RvyQmnXrpCkMXg3d"
   ];
 }
-  (lib.mkIf (config.networking.hostName == "NixOS") {
+  (lib.mkIf (builtins.elem config.networking.hostName testHosts) {
     users.users.root.password = ""; # for the live ISO only
   })
-  (lib.mkIf (config.networking.hostName != "NixOS") {
+  (lib.mkIf (!builtins.elem config.networking.hostName testHosts) {
     age.secrets.rootPasswd.file = "${self}/secrets/passwd_root.age";
     users.users.root.passwordFile = "/run/agenix/rootPasswd.age";
   })]
