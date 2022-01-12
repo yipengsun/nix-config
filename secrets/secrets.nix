437b968f510f4fb56d20d0b35694b3a6d3d90ad3
Yipeng Sun <syp@umd.edu>
2022-01-12 04:17:17 +0100

    Add my passwords encrypted w/ agenix.

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,9 @@
 let
   # set ssh public keys here for your system and user
-  system = "";
-  user = "";
-  allKeys = [ system user ];
+  user = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC1s0sy5xORVQZcM7Yg1UcxqxGOazY41kci43OV0aqX7owjrxJKhezeOU0uehcvr2uaJykF5wRphaMjiY5tmaVyh35RKZ7tu5B7bx0FOjgATrUFAcBgKqzVMeCSmvmSUNK02HYrP+SOWbdgYECkyF+7PVxZoUefPnpBfGiqunfBWD5YrJMJPToFRqRW7Lcl+/6wIZQOAvPq8lvhfG89r9SvdiEX8umpYJKRgIl9k5wOsimTFJ5wLfq39sjECIzGCcbVLkiPzkOPLWRRgamICbiN4f0HF8kqdDU0mD1WZ5wHM72P68WKpHhMn9l+NEsGYik0fkW+RvyQmnXrpCkMXg3d";
+  allKeys = [ user ];
 in
 {
-  "secret.age".publicKeys = allKeys;
+  "passwd_root.age".publicKeys = allKeys;
+  "passwd_syp.age".publicKeys = allKeys;
 }
