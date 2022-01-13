33d61013c5d3db60f48025da0593ec507bcd1836
Yipeng Sun <syp@umd.edu>
2022-01-13 14:30:14 +0100

    Store direnvs centrally.

--- local/profiles/direnv/default.nix
+++ local/profiles/direnv/default.nix
@@ -4,5 +4,12 @@
     nix-direnv = {
       enable = true;
     };
+
+    # Store direnvs centrally
+    stdlib = ''
+      : ''${XDG_CACHE_HOME:=''$HOME/.cache}
+      pwd_hash=''$(echo -n ''$PWD | sha1sum | cut -d ' ' -f 1)
+      direnv_layout_dir=''$XDG_CACHE_HOME/direnv/''$pwd_hash
+    '';
   };
 }
