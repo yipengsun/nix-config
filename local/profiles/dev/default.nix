f9474cb90f8cf04d617c369385ee84b179717b95
Yipeng Sun <syp@umd.edu>
2022-01-14 17:57:41 +0100

    Add Python linter global config.

--- local/profiles/dev/default.nix
+++ local/profiles/dev/default.nix
@@ -9,4 +9,8 @@
     texlab
     rnix-lsp
   ];
+
+  # linter config
+  xdg.configFile."flake8".text = builtins.readFile ./flake8;
+  xdg.configFile."pylintrc".text = builtins.readFile ./pylintrc;
 }
