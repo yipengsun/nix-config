f421a623909564ad1e76606b18b54f308c766b24
Yipeng Sun <syp@umd.edu>
2022-01-19 03:18:21 +0100

    WIP #8: All extra ENV vars and shell commands; launch xinit manually.

--- local/modules/window-manager.nix
+++ local/modules/window-manager.nix
@@ -10,6 +10,8 @@ let
   makeSearchPath = lib.concatMapStrings (path:
     " --search ${getLuaPath path "share"}"
     + " --search ${getLuaPath path "lib"}");
+
+  envVarsStr = config.lib.zsh.exportAll cfg.envVars;
 in
 
 {
@@ -22,6 +24,18 @@ in
         default = pkgs.awesome;
         defaultText = literalExpression "pkgs.awesome";
         description = "Package to use for running the Awesome WM.";
+      };
+
+      envVars = mkOption {
+        type = types.attrs;
+        default = { };
+        description = "List of ENV vars, with name and value";
+      };
+
+      initExtra = mkOption {
+        type = types.lines;
+        default = "";
+        description = "Extra shell commands to run during initialization.";
       };
 
       luaModules = mkOption {
@@ -49,6 +63,12 @@ in
       if [ -f $HOME/.Xdefaults ]; then
         xrdb $HOME/.Xdefaults
       fi
+
+      # export ENV vars
+      ${envVarsStr}
+
+      # extra commands
+      ${cfg.initExtra}
 
       # start awesome window manager
       ${awesome}/bin/awesome ${makeSearchPath cfg.luaModules}
--- local/profiles/wm/default.nix
+++ local/profiles/wm/default.nix
@@ -6,5 +6,20 @@
     luaModules = with pkgs; [
       luaPackages.vicious
     ];
+
+    envVars = {
+      XMODIFIERS = "@im=fcitx";
+      GTK_IM_MODULE = "fcitx";
+      QT_IM_MODULE = "xim";
+    };
+
+    initExtra = ''
+      # TrackPoint settings
+      tpset() { xinput set-prop "TPPS/2 ALPS TrackPoint" "$@"; }
+      tpset "Evdev Wheel Emulation" 1
+      tpset "Evdev Wheel Emulation Buttom" 2
+
+      fcitx -r
+    '';
   };
 }
