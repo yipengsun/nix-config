3ecb8aee43f999ca6c5f8e18cd7bfc79f97d1646
Yipeng Sun <syp@umd.edu>
2022-01-19 20:43:54 +0100

    Strange, fcitx profiles were not getting linked.

--- local/profiles/wm/default.nix
+++ local/profiles/wm/default.nix
@@ -2,9 +2,7 @@
 {
   # sadly you need to put this line in your host setting manually:
   #   services.xserver.displayManager.startx.enable = true;
-  xinit = {
-    enable = true;
-  };
+  xinit.enable = true;
 
   xinit.windowManager.awesome = {
     enable = true;
@@ -14,14 +12,6 @@
     ];
   };
 
-  # lets also define programs that run with X here
-  i18n.inputMethod.enabled = "fcitx5";
-  i18n.inputMethod.fcitx5.addons = with pkgs; [
-    fcitx5-chinese-addons
-    fcitx5-material-color
-    fcitx5-pinyin-zhwiki # huge Chinese dict for pinyin input
-  ];
-
   # copy fcitx5 config
   xdg.configFile."fcitx5/config".source = ./fcitx5/config;
   xdg.configFile."fcitx5/profile".source = ./fcitx5/profile;
@@ -29,6 +19,14 @@
   xdg.configFile."fcitx5/conf/pinyin.conf".source = ./fcitx5/conf/pinyin.conf;
   xdg.configFile."fcitx5/conf/cloudpinyin.conf".source = ./fcitx5/conf/cloudpinyin.conf;
   xdg.configFile."fcitx5/conf/punctuation.conf".source = ./fcitx5/conf/punctuation.conf;
+
+  # lets also define programs that run with X here
+  i18n.inputMethod.enabled = "fcitx5";
+  i18n.inputMethod.fcitx5.addons = with pkgs; [
+    fcitx5-chinese-addons
+    fcitx5-material-color
+    fcitx5-pinyin-zhwiki # huge Chinese dict for pinyin input
+  ];
 
   # enable dropbox
   services.dropbox-autoreconnect.enable = true;
