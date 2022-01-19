8d7a5c5e5760d1811749a3e28d7b5ba0a5afd388
Yipeng Sun <syp@umd.edu>
2022-01-19 17:56:22 +0100

    Fix #9: Manage most of fcitx5's config via hm.

--- local/profiles/wm/default.nix
+++ local/profiles/wm/default.nix
@@ -14,9 +14,19 @@
     ];
   };
 
+  # lets also define programs that run with X here
   i18n.inputMethod.enabled = "fcitx5";
   i18n.inputMethod.fcitx5.addons = with pkgs; [
     fcitx5-chinese-addons
     fcitx5-material-color
+    fcitx5-pinyin-zhwiki # huge Chinese dict for pinyin input
   ];
+
+  # copy fcitx5 config
+  xdg.configFile."fcitx5/config".source = ./fcitx5/config;
+  xdg.configFile."fcitx5/profile".source = ./fcitx5/profile;
+  xdg.configFile."fcitx5/conf/classicui.conf".source = ./fcitx5/conf/classicui.conf;
+  xdg.configFile."fcitx5/conf/pinyin.conf".source = ./fcitx5/conf/pinyin.conf;
+  xdg.configFile."fcitx5/conf/cloudpinyin.conf".source = ./fcitx5/conf/cloudpinyin.conf;
+  xdg.configFile."fcitx5/conf/punctuation.conf".source = ./fcitx5/conf/punctuation.conf;
 }
