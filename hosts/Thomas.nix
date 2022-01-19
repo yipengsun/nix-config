6f0f1c54b771a15139654e2950835efd55c7bc61
Yipeng Sun <syp@umd.edu>
2022-01-19 16:13:08 +0100

    Move the TrackPoint settings to host.
    Damn Lenovo, you really ruined my beloved TrackPoint on newer ThinkPads!

--- hosts/Thomas.nix
+++ hosts/Thomas.nix
@@ -128,6 +128,18 @@
 
   imports = suites.laptop;
 
+  home-manager.users.syp.xinit.initExtra = ''
+    # TrackPoint settings
+    tpset() { xinput set-prop "TPPS/2 ALPS TrackPoint" "$@"; }
+
+    tpset "Evdev Wheel Emulation" 1
+    tpset "Evdev Wheel Emulation Button" 2
+    tpset "Evdev Wheel Emulation Axes" 6 7 4 5
+    tpset "Evdev Wheel Emulation Timeout" 200
+    tpset "Device Accel Profile" -1
+    tpset "Device Accel Constant Deceleration" 0.5
+  '';
+
   ################
   # Legacy stuff #
   ################
--- local/profiles/wm/default.nix
+++ local/profiles/wm/default.nix
@@ -1,19 +1,9 @@
 { pkgs, ... }:
 {
+  # sadly you need to put this line in your host setting manually:
+  #   services.xserver.displayManager.startx.enable = true;
   xinit = {
     enable = true;
-
-    initExtra = ''
-      # TrackPoint settings
-      tpset() { xinput set-prop "TPPS/2 ALPS TrackPoint" "$@"; }
-
-      tpset "Evdev Wheel Emulation" 1
-      tpset "Evdev Wheel Emulation Button" 2
-      tpset "Evdev Wheel Emulation Axes" 6 7 4 5
-      tpset "Evdev Wheel Emulation Timeout" 200
-      tpset "Device Accel Profile" -1
-      tpset "Device Accel Constant Deceleration" 0.5
-    '';
   };
 
   xinit.windowManager.awesome = {
