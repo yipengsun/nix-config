d3268ce6c3feb6d2cdf56168333582fa476e5bf9
Yipeng Sun <syp@umd.edu>
2022-01-23 15:27:14 -0500

    Make fcitx config out-of-store.

--- local/profiles/wm/default.nix
+++ local/profiles/wm/default.nix
@@ -2,8 +2,12 @@
 
 let
   customLuaPackages = pkgs.lua53Packages;
+
   weatherApiKeyLoc = "${config.xdg.configHome}/awesome/weather_api_key";
+
   mkOut = config.lib.file.mkOutOfStoreSymlink;
+  stupidPath = path: "${config.home.homeDirectory}/src/nix-config/local/profiles/wm/" + path;
+  # FIXME: this project now has to be placed at ~/src/nix-config
 in
 
 {
@@ -31,12 +35,15 @@ in
   };
 
   # copy fcitx5 config out of store
-  xdg.configFile."fcitx5/config".source = mkOut ./fcitx5/config;
-  xdg.configFile."fcitx5/profile".source = mkOut ./fcitx5/profile;
-  xdg.configFile."fcitx5/conf/classicui.conf".source = mkOut ./fcitx5/conf/classicui.conf;
-  xdg.configFile."fcitx5/conf/pinyin.conf".source = mkOut ./fcitx5/conf/pinyin.conf;
-  xdg.configFile."fcitx5/conf/cloudpinyin.conf".source = mkOut ./fcitx5/conf/cloudpinyin.conf;
-  xdg.configFile."fcitx5/conf/punctuation.conf".source = mkOut ./fcitx5/conf/punctuation.conf;
+  # FIXME: this is an ugly workaround to make hm links out-of-store
+  #        see https://github.com/nix-community/home-manager/issues/2085#issuecomment-861740971
+  #        somehow the 'builtins.toString' method doesn't work?!
+  xdg.configFile."fcitx5/config".source = mkOut (stupidPath "fcitx5/config");
+  xdg.configFile."fcitx5/profile".source = mkOut (stupidPath "fcitx5/profile");
+  xdg.configFile."fcitx5/conf/classicui.conf".source = mkOut (stupidPath "fcitx5/conf/classicui.conf");
+  xdg.configFile."fcitx5/conf/pinyin.conf".source = mkOut (stupidPath "fcitx5/conf/pinyin.conf");
+  xdg.configFile."fcitx5/conf/cloudpinyin.conf".source = mkOut (stupidPath "fcitx5/conf/cloudpinyin.conf");
+  xdg.configFile."fcitx5/conf/punctuation.conf".source = mkOut (stupidPath "fcitx5/conf/punctuation.conf");
 
   # lets also define programs that run with X here
   i18n.inputMethod.enabled = "fcitx5";
