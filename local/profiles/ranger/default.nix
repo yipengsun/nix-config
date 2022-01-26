f0b1e55bb4a9d8d911abf27b324326b89a0534f7
Yipeng Sun <syp@umd.edu>
2022-01-26 15:45:22 -0500

    WIP: Trying to wrap ueberzug s.t. it works in a vitualenv.
    Not working yet.

--- local/profiles/ranger/default.nix
+++ local/profiles/ranger/default.nix
@@ -1,6 +1,20 @@
 { pkgs, ... }:
+
+let
+  rangerWrapped = pkgs.symlinkJoin {
+    name = "ranger-bundle";
+    paths = [ pkgs.ranger pkgs.ueberzug ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      for prog in ranger rifle ueberzug; do
+        wrapProgram $out/bin/$prog --prefix PYTHONPATH : $out/lib
+      done
+    '';
+  };
+in
+
 {
-  home.packages = [ pkgs.ranger pkgs.ueberzug ];
+  home.packages = [ rangerWrapped ];
 
   xdg.configFile."ranger/rc.conf".source = ./rc.conf;
 }
