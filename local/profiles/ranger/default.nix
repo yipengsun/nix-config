16361f5af7bfa49cc4a5e194ddad67bce0992c8b
Yipeng Sun <syp@umd.edu>
2022-01-27 11:36:43 -0500

    Fix: Set PYTHONPATH instead of prefix it made ueberzug work in venv.

--- local/profiles/ranger/default.nix
+++ local/profiles/ranger/default.nix
@@ -1,20 +1,20 @@
 { pkgs, ... }:
 
 let
-  rangerWrapped = pkgs.symlinkJoin {
+  rangerBundle = pkgs.symlinkJoin {
     name = "ranger-bundle";
     paths = [ pkgs.ranger pkgs.ueberzug ];
     buildInputs = [ pkgs.makeWrapper ];
     postBuild = ''
       for prog in ranger rifle ueberzug; do
-        wrapProgram $out/bin/$prog --prefix PYTHONPATH : $out/lib
+        wrapProgram "$out/bin/$prog" --set PYTHONPATH "$out/lib"
       done
     '';
   };
 in
 
 {
-  home.packages = [ rangerWrapped ];
+  home.packages = [ rangerBundle ];
 
   xdg.configFile."ranger/rc.conf".source = ./rc.conf;
 }
