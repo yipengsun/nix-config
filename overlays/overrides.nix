0e921a7783fd050f636c29fb95d83e9433edc135
Yipeng Sun <syp@umd.edu>
2022-01-27 17:03:38 -0500

    Fully wrap root aux programs.

--- overlays/overrides.nix
+++ overlays/overrides.nix
@@ -21,4 +21,15 @@ channels: final: prev: {
             haskell-language-server;
         });
     });
+
+  root = prev.root.overrideAttrs
+    (old: rec {
+      postInstall = ''
+        for prog in rootbrowse rootcp rooteventselector rootls rootmkdir rootmv rootprint rootrm rootslimtree; do
+          wrapProgram "$out/bin/$prog" \
+            --set PYTHONPATH "$out/lib" \
+            --set LD_LIBRARY_PATH "$out/lib"
+        done
+      '';
+    });
 }
