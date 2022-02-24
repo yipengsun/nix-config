b0723e0d7a67e15c0f8ef987459e06dcff641582
Yipeng Sun <syp@umd.edu>
2022-02-24 01:20:42 -0500

    Bump nixpkgs; disable obsolete overrides.

--- overlays/overrides.nix
+++ overlays/overrides.nix
@@ -19,26 +19,15 @@ channels: final: prev: {
     ];
   };
 
-  haskellPackages = prev.haskellPackages.override
-    (old: {
-      overrides = prev.lib.composeExtensions (old.overrides or (_: _: { })) (hfinal: hprev:
-        let version = prev.lib.replaceChars [ "." ] [ "" ] prev.ghc.version;
-        in
-        {
-          # same for haskell packages, matching ghc versions
-          inherit (channels.latest.haskell.packages."ghc${version}")
-            haskell-language-server;
-        });
-    });
-
-  root = prev.root.overrideAttrs
-    (old: rec {
-      postInstall = ''
-        for prog in rootbrowse rootcp rooteventselector rootls rootmkdir rootmv rootprint rootrm rootslimtree; do
-          wrapProgram "$out/bin/$prog" \
-            --set PYTHONPATH "$out/lib" \
-            --set LD_LIBRARY_PATH "$out/lib"
-        done
-      '';
-    });
+  # haskellPackages = prev.haskellPackages.override
+  #   (old: {
+  #     overrides = prev.lib.composeExtensions (old.overrides or (_: _: { })) (hfinal: hprev:
+  #       let version = prev.lib.replaceChars [ "." ] [ "" ] prev.ghc.version;
+  #       in
+  #       {
+  #         # same for haskell packages, matching ghc versions
+  #         inherit (channels.latest.haskell.packages."ghc${version}")
+  #           haskell-language-server;
+  #       });
+  #   });
 }
