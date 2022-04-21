e1e7b1535e3ddc3b825e32a3435ba6f16f3421ff
Yipeng Sun <syp@umd.edu>
2022-04-21 14:56:53 -0400

    Need to install HP printer driver.

--- global/profiles/printer/default.nix
+++ global/profiles/printer/default.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 {
   services.printing.enable = true;
+  services.printing.drivers = [ pkgs.hplip ]; # For UMD printers
 
   services.avahi.enable = true;
   services.avahi.nssmdns = true;
