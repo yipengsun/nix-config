b3e382b3c23697f39c65d35a7e14cb13a522b3f4
Yipeng Sun <syp@umd.edu>
2022-10-09 19:53:57 -0400

    Fewer zfs snapshots.

--- global/profiles/zfs/default.nix
+++ global/profiles/zfs/default.nix
@@ -2,9 +2,9 @@
   services.zfs.trim.enable = true; # trim SSD periodically
   services.zfs.autoSnapshot = {
     enable = true;
-    frequent = 3;
-    daily = 3;
-    weekly = 2;
-    monthly = 2;
+    frequent = 1;
+    daily = 1;
+    weekly = 1;
+    monthly = 1;
   };
 }
