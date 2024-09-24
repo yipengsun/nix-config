f085f6508ebe684b9bd43ce499863d9e68aaf9a0
Yipeng Sun <syp@umd.edu>
2024-09-24 18:50:31 +0800

    Fix: migrate obsoleted vim plugin config.
    More specifically, that of indent-blankline.nvim.

--- profiles/home/neovim/default.nix
+++ profiles/home/neovim/default.nix
@@ -266,11 +266,7 @@ in
               vim.opt.listchars:append("space:⋅")
               vim.opt.listchars:append("eol:↴")
 
-              require("indent_blankline").setup {
-                  space_char_blankline = " ",
-                  show_current_context = true,
-                  show_current_context_start = true,
-              }
+              require("ibl").setup()
             EOF
           '';
         }
