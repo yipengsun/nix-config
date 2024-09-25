e62422f0c0905c13af77932a78b93a7d23fc0404
Yipeng Sun <syp@umd.edu>
2024-09-25 15:45:09 +0800

    Tweaking my fish.

--- profiles/home/fish/default.nix
+++ profiles/home/fish/default.nix
@@ -11,7 +11,7 @@
 
       # nix
       ns = "nix search --no-update-lock-file";
-      nepl = "nix repl '<nixpkgs>'";
+      nepl = "nix repl --file '<nixpkgs>'";
       nsysgen = "sudo nix-env --list-generations --profile /nix/var/nix/profiles/system";
       nstrayroots = ''nix-store --gc --print-roots | egrep -v "^(/nix/var|/run/\w+-system|\{memory)"'';
 
@@ -44,16 +44,38 @@
       end
 
       bind . 'expand-dot-to-parent-directory-path'
+
+      # jump between words with ctrl-j/ctrl-k
+      bind \cj backward-word
+      bind \ck forward-word
     '';
 
     plugins = [
       {
-        name = "fish";
+        name = "pure";
         src = pkgs.fetchFromGitHub {
           owner = "pure-fish";
           repo = "pure";
           rev = "28447d2e7a4edf3c954003eda929cde31d3621d2";
           sha256 = "sha256-8zxqPU9N5XGbKc0b3bZYkQ3yH64qcbakMsHIpHZSne4=";
+        };
+      }
+      {
+        name = "z";
+        src = pkgs.fetchFromGitHub {
+          owner = "jethrokuan";
+          repo = "z";
+          rev = "85f863f20f24faf675827fb00f3a4e15c7838d76";
+          sha256 = "sha256-+FUBM7CodtZrYKqU542fQD+ZDGrd2438trKM0tIESs0=";
+        };
+      }
+      {
+        name = "fzf.fish";
+        src = pkgs.fetchFromGitHub {
+          owner = "PatrickF1";
+          repo = "fzf.fish";
+          rev = "8920367cf85eee5218cc25a11e209d46e2591e7a";
+          sha256 = "sha256-T8KYLA/r/gOKvAivKRoeqIwE2pINlxFQtZJHpOy9GMM=";
         };
       }
     ];
