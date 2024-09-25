a141eed182293cac2452c9814d6fe097a4fd72cf
Yipeng Sun <syp@umd.edu>
2024-09-25 09:15:33 +0800

    Emit OSC 7 on zsh startup; remove zsh custom syntax hilighting.

--- profiles/home/zsh/default.nix
+++ profiles/home/zsh/default.nix
@@ -108,43 +108,6 @@
 
         bindkey '\e\e' sudo-command-line
 
-        # Custom zsh syntax highlight
-        typeset -A ZSH_HIGHLIGHT_STYLES
-
-        ZSH_HIGHLIGHT_STYLES[default]='bold'
-        ZSH_HIGHLIGHT_STYLES[reserved-word]='fg=yellow,bold'
-        ZSH_HIGHLIGHT_STYLES[suffix-alias]='fg=yellow,underline,bold'
-        ZSH_HIGHLIGHT_STYLES[global-alias]='fg=cyan,bold'
-        ZSH_HIGHLIGHT_STYLES[precommand]='fg=yellow,underline,bold'
-        ZSH_HIGHLIGHT_STYLES[commandseparator]='bold'
-        ZSH_HIGHLIGHT_STYLES[autodirectory]='fg=yellow,underline,bold'
-        ZSH_HIGHLIGHT_STYLES[path]='underline,bold'
-        ZSH_HIGHLIGHT_STYLES[path_pathseparator]='bold'
-        ZSH_HIGHLIGHT_STYLES[path_prefix_pathseparator]='bold'
-        ZSH_HIGHLIGHT_STYLES[globbing]='fg=blue,bold'
-        ZSH_HIGHLIGHT_STYLES[history-expansion]='fg=blue,bold'
-        ZSH_HIGHLIGHT_STYLES[command-substitution]='bold'
-        ZSH_HIGHLIGHT_STYLES[command-substitution-delimiter]='fg=magenta,bold'
-        ZSH_HIGHLIGHT_STYLES[process-substitution]='bold'
-        ZSH_HIGHLIGHT_STYLES[process-substitution-delimiter]='fg=magenta,bold'
-        ZSH_HIGHLIGHT_STYLES[single-hyphen-option]='bold'
-        ZSH_HIGHLIGHT_STYLES[double-hyphen-option]='bold'
-        ZSH_HIGHLIGHT_STYLES[back-quoted-argument]='bold'
-        ZSH_HIGHLIGHT_STYLES[back-quoted-argument-delimiter]='fg=magenta,bold'
-        ZSH_HIGHLIGHT_STYLES[single-quoted-argument]='fg=yellow,bold'
-        ZSH_HIGHLIGHT_STYLES[double-quoted-argument]='fg=yellow,bold'
-        ZSH_HIGHLIGHT_STYLES[dollar-quoted-argument]='fg=yellow,bold'
-        ZSH_HIGHLIGHT_STYLES[rc-quote]='fg=cyan,bold'
-        ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]='fg=cyan,bold'
-        ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]='fg=cyan,bold'
-        ZSH_HIGHLIGHT_STYLES[back-dollar-quoted-argument]='fg=cyan,bold'
-        ZSH_HIGHLIGHT_STYLES[assign]='bold'
-        ZSH_HIGHLIGHT_STYLES[redirection]='fg=yellow,bold'
-        ZSH_HIGHLIGHT_STYLES[comment]='fg=black,bold'
-        ZSH_HIGHLIGHT_STYLES[named-fd]='bold'
-        ZSH_HIGHLIGHT_STYLES[numeric-fd]='bold'
-        ZSH_HIGHLIGHT_STYLES[arg0]='fg=green,bold'
-
         # Emit OSC 7 for wezterm to consume
         urlencode() {
           local length="''${#1}"
@@ -163,6 +126,9 @@
 
         autoload -Uz add-zsh-hook
         add-zsh-hook -Uz chpwd osc7_cwd
+
+        # run osc7_cwd on zsh startup
+        osc7_cwd
       '';
 
       prezto = {
