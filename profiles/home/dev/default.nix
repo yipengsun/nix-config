9e91da4df94042a6fa8b9db024051ab5ef85b3d5
Yipeng Sun <syp@umd.edu>
2024-09-25 09:32:34 +0800

    No need to export system-wide Python linter config.

--- profiles/home/dev/default.nix
+++ profiles/home/dev/default.nix
@@ -9,14 +9,11 @@
     nixpkgs-review # for reviewing nixpkgs pr
     nixpkgs-fmt # code formatter
     nix-tree # view dependency as a tree
-    black # python code formatter
     #jetbrains.clion
     gh # github cli tool
     nix-tree
   ];
 
   # linter config
-  # flake8 4.0+ no longer support a user-wide config!
-  #xdg.configFile."flake8".text = builtins.readFile ./flake8;
-  xdg.configFile."pylintrc".text = builtins.readFile ./pylintrc;
+  #xdg.configFile."pylintrc".text = builtins.readFile ./pylintrc;
 }
