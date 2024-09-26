d26618455c565ba8eee0924af2a85142ff1c99cb
Yipeng Sun <syp@umd.edu>
2024-09-26 19:00:01 +0800

    Add a secret to allow nix to access github authenticated.

--- profiles/home/dev-secrets/default.nix
+++ profiles/home/dev-secrets/default.nix
@@ -1,7 +1,15 @@
 { config, ... }: {
-  age.secrets.netrc = {
-    file = ../../../secrets/netrc_syp.age;
-    path = "${config.home.homeDirectory}/.netrc";
-    mode = "600";
+  age.secrets = {
+    netrc = {
+      file = ../../../secrets/netrc_syp.age;
+      path = "${config.home.homeDirectory}/.netrc";
+      mode = "600";
+    };
+
+    nix_conf = {
+      file = ../../../secrets/nix_conf_syp.age;
+      path = "${config.home.homeDirectory}/.config/nix/nix.conf";
+      mode = "600";
+    };
   };
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,7 +2,9 @@ let
   # set ssh public keys here for your system and user
   hostThomas = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPziJHazosCbvSaSRO6voEniAdQCx2Fb+BDpq9umiSCD";
   hostHenri = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEPmHuWIMKKPdqt7FY3mVh0n2skSPeg11zX0BP9OAbYp";
+
   userSyp = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC1s0sy5xORVQZcM7Yg1UcxqxGOazY41kci43OV0aqX7owjrxJKhezeOU0uehcvr2uaJykF5wRphaMjiY5tmaVyh35RKZ7tu5B7bx0FOjgATrUFAcBgKqzVMeCSmvmSUNK02HYrP+SOWbdgYECkyF+7PVxZoUefPnpBfGiqunfBWD5YrJMJPToFRqRW7Lcl+/6wIZQOAvPq8lvhfG89r9SvdiEX8umpYJKRgIl9k5wOsimTFJ5wLfq39sjECIzGCcbVLkiPzkOPLWRRgamICbiN4f0HF8kqdDU0mD1WZ5wHM72P68WKpHhMn9l+NEsGYik0fkW+RvyQmnXrpCkMXg3d";
+
   allKeys = [ hostThomas hostHenri userSyp ];
 in
 {
@@ -13,4 +15,5 @@ in
 
   "weather_api_key.age".publicKeys = [ userSyp ];
   "netrc_syp.age".publicKeys = [ userSyp ];
+  "nix_conf_syp.age".publicKeys = [ userSyp ];
 }
