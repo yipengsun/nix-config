823038dc028ed72262e84d006169155c44edfcb5
Yipeng Sun <syp@umd.edu>
2024-09-27 13:52:15 +0800

    tmux: use similar window-splitting key bindings as in wezterm.
    wezterm -> tmux: Ctrl-Alt-\ -> Ctrl-A-\
                     Ctrl-Alt-' -> Ctrl-A-'

--- profiles/home/tmux/default.nix
+++ profiles/home/tmux/default.nix
@@ -1,9 +1,10 @@
-{ pkgs, ... }: {
+{
   programs.tmux = {
     enable = true;
     clock24 = true;
     baseIndex = 1;
     escapeTime = 250;
+    terminal = "tmux-256color";
     extraConfig = builtins.readFile ./tmux.conf;
   };
 }
