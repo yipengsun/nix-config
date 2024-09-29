627580392eca4bb3cebda1c492d162e99c3e59e9
Yipeng Sun <syp@umd.edu>
2024-09-29 23:25:56 +0800

    Refactor to make inputs more obvious.

--- profiles/nixos/encfs-automount/default.nix
+++ profiles/nixos/encfs-automount/default.nix
@@ -1,9 +1,15 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  user = "syp";
+  encryptedDir = "/home/syp/sync/dropbox/data";
+  mountDir = "~/data";
+in
+{
   # set up pam mount
   security.pam.mount.additionalSearchPaths = [ pkgs.encfs ];
   security.pam.mount.extraVolumes = [
     # for me only, don't do this for root!
-    ''<volume user="syp" fstype="fuse" path="encfs#/home/syp/sync/dropbox/data" mountpoint="~/data" options="nonempty" />''
+    ''<volume user="${user}" fstype="fuse" path="encfs#${encryptedDir}" mountpoint="${mountDir}" options="nonempty" />''
   ];
 
   # automount via pam_mount on login
