9fc58cf99b751de4e8c42d7a84f018c747a3d454
Yipeng Sun <syp@umd.edu>
2024-09-30 02:09:43 +0800

    encfs: fix: auto mount on login.
    The name of the enable option was changed.

--- profiles/nixos/encfs-automount/default.nix
+++ profiles/nixos/encfs-automount/default.nix
@@ -5,13 +5,11 @@ let
   mountDir = "~/data";
 in
 {
-  # set up pam mount
+  # automount via pam_mount on login
+  security.pam.mount.enable = true;
   security.pam.mount.additionalSearchPaths = [ pkgs.encfs ];
   security.pam.mount.extraVolumes = [
     # for me only, don't do this for root!
     ''<volume user="${user}" fstype="fuse" path="encfs#${encryptedDir}" mountpoint="${mountDir}" options="nonempty" />''
   ];
-
-  # automount via pam_mount on login
-  security.pam.services.login.pamMount = true;
 }
