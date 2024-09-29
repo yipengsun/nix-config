1363255478b09454bc89c93813e4bf36a85d2d16
Yipeng Sun <syp@umd.edu>
2024-09-29 15:05:35 +0800

    dropbox: disable for now as it's not working.

--- profiles/home/wm-x11/default.nix
+++ profiles/home/wm-x11/default.nix
@@ -2,5 +2,6 @@
   awesome-wm-config.enable = true;
   fcitx5-config.enable = true;
 
-  services.dropbox.enable = true;
+  # FIXME: dropbox doesn't work on unstable nixpkgs as of 2024-09-29
+  #services.dropbox.enable = true;
 }
