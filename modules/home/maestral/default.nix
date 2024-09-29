0b571ea627ddd2a9cb0ecd22d4ec134d4446e38e
Yipeng Sun <syp@umd.edu>
2024-09-30 01:36:06 +0800

    maestral: fix #18: settle on a user systemd service.
    This allows me to use `maestral status` normally,
    but provides no guarantee on starting maestral after network is available.

---
+++ modules/home/maestral/default.nix
@@ -0,0 +1,44 @@
+# stolen from:
+#   https://github.com/NixOS/nixpkgs/issues/235345#issuecomment-1586233679
+#   https://github.com/NixOS/nixpkgs/issues/235345#issuecomment-1622892967
+{ config
+, lib
+, pkgs
+, ...
+}:
+with lib; let
+  cfg = config.services.maestral;
+in
+{
+  options.services.maestral = {
+    enable = mkEnableOption "enable Maestral, a open-source Dropbox client.";
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = [ pkgs.maestral ];
+
+    systemd.user.services.maestral = {
+      Unit = rec {
+        Description = "Maestral - a open-source Dropbox client";
+
+        After = [
+          "graphical-session.target"
+        ];
+        Requires = After;
+      };
+
+      Service = {
+        Type = "notify";
+
+        ExecStart = "${pkgs.maestral}/bin/maestral start --foreground";
+        ExecStop = "${pkgs.maestral}/bin/maestral stop";
+
+        Nice = 10;
+        Restart = "on-failure";
+        RestartSec = "5s";
+      };
+
+      Install.WantedBy = [ "graphical-session.target" ];
+    };
+  };
+}
