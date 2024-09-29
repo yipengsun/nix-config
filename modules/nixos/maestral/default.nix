9fb5391f5b68224583045eb19452bd23d9cee076
Yipeng Sun <syp@umd.edu>
2024-09-30 01:20:22 +0800

    maestral: wip #18: service is running but can't get status with `maestral status`.

--- modules/nixos/maestral/default.nix
+++ modules/nixos/maestral/default.nix
@@ -9,6 +9,9 @@
 }:
 with lib; let
   cfg = config.services.maestral;
+
+  userHome = config.users.users.${cfg.user}.home;
+  userUid = builtins.toString config.users.users.${cfg.user}.uid;
 in
 {
   options.services.maestral = {
@@ -23,23 +26,35 @@ in
 
     systemd.services."maestral@${cfg.user}" = {
       description = "Maestral - a open-source Dropbox client";
+      wantedBy = [ "multi-user.target" ];
+
+      environment = {
+        XDG_RUNTIME_DIR = "${userHome}/.cache";
+        DBUS_SESSION_BUS_ADDRESS = "unix:path=/run/user/${userUid}/bus";
+      };
 
       unitConfig = rec {
         After = [
           "network-online.target"
         ];
         Requires = After;
+        RequiresMountsFor = config.users.users.${cfg.user}.home;
 
         ConditionPathExists = [
-          "${config.users.users.${cfg.user}.home}/.config/maestral/maestral.ini"
+          "${userHome}/.config/maestral/maestral.ini"
         ];
       };
 
       serviceConfig = {
+        Type = "notify";
+
         ExecStart = "${pkgs.maestral}/bin/maestral start --foreground";
         ExecStop = "${pkgs.maestral}/bin/maestral stop";
+
         Nice = 10;
         Restart = "on-failure";
+        RestartSec = "5s";
+
         User = cfg.user;
         Group = config.users.users.${cfg.user}.group;
         WorkingDirectory = "~";
--- users/syp.nix
+++ users/syp.nix
@@ -13,6 +13,7 @@
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC1s0sy5xORVQZcM7Yg1UcxqxGOazY41kci43OV0aqX7owjrxJKhezeOU0uehcvr2uaJykF5wRphaMjiY5tmaVyh35RKZ7tu5B7bx0FOjgATrUFAcBgKqzVMeCSmvmSUNK02HYrP+SOWbdgYECkyF+7PVxZoUefPnpBfGiqunfBWD5YrJMJPToFRqRW7Lcl+/6wIZQOAvPq8lvhfG89r9SvdiEX8umpYJKRgIl9k5wOsimTFJ5wLfq39sjECIzGCcbVLkiPzkOPLWRRgamICbiN4f0HF8kqdDU0mD1WZ5wHM72P68WKpHhMn9l+NEsGYik0fkW+RvyQmnXrpCkMXg3d"
     ];
+    uid = 1000;
   };
 
   home-manager.users.syp = { config, ... }: {
