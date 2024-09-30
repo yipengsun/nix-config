5a05d4367d692296905709f0250814e9c59dce93
Yipeng Sun <syp@umd.edu>
2024-09-30 10:12:54 +0800

    gocryptfs: fix #22: enable auto mount on login.

--- profiles/nixos/gocryptfs-automount/default.nix
+++ profiles/nixos/gocryptfs-automount/default.nix
@@ -3,15 +3,15 @@ let
   user = "syp";
   encryptedDir = "/home/%(USER)/sync/dropbox/data";
   mountDir = "/home/%(USER)/data";
-  cryptor = pkgs.encfs;
+  cryptor = pkgs.gocryptfs;
 in
 {
   environment.systemPackages = [ cryptor ];
 
   # automount via pam_mount on login
-  security.pam.mount.enable = false;
+  security.pam.mount.enable = true;
   security.pam.mount.additionalSearchPaths = [ cryptor ];
   security.pam.mount.extraVolumes = [
-    ''<volume user="${user}" fstype="fuse" path="encfs#${encryptedDir}" mountpoint="${mountDir}" options="nonempty" />''
+    ''<volume user="${user}" fstype="fuse" path="gocryptfs#${encryptedDir}" mountpoint="${mountDir}" options="nodev,nosuid,quiet" />''
   ];
 }
