b1b9b75f20c64beb6ca2cf6f3fc5c13a14872819
Yipeng Sun <syp@umd.edu>
2024-09-30 23:46:54 +0800

    picom: enable for some visual aids.
    I try to use Dracula everywhere; now fcitx and terminal have the same background,
    making it very hard to tell the input candidates from terminal text.

--- profiles/home/wm-x11/default.nix
+++ profiles/home/wm-x11/default.nix
@@ -5,4 +5,12 @@
   # FIXME: dropbox doesn't work on unstable nixpkgs as of 2024-09-29
   #services.dropbox.enable = true;
   services.maestral.enable = true;
+
+  services.picom = {
+    enable = true;
+    shadow = true;
+    shadowExclude = [
+      "_NET_WM_WINDOW_TYPE@:32a * = '_NET_WM_WINDOW_TYPE_DOCK'"
+    ];
+  };
 }
