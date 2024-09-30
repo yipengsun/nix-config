bcc55a8f43bf8ad01303f508b53b9a9892151b4f
Yipeng Sun <syp@umd.edu>
2024-09-30 20:18:49 +0800

    encfs: wip #22: test encfs auto-mount w/ pam_mount.
    This setup works. So something's wrong with gocryptfs.

--- profiles/nixos/encfs-automount/default.nix
+++ profiles/nixos/encfs-automount/default.nix
@@ -1,17 +1,19 @@
 { pkgs, ... }:
 let
   user = "syp";
-  encryptedDir = "/home/%(USER)/sync/dropbox/data";
+  encryptedDir = "/home/%(USER)/tmp/encfs_test";
   mountDir = "/home/%(USER)/data";
   cryptor = pkgs.encfs;
 in
 {
   environment.systemPackages = [ cryptor ];
+  # init a new encfs folder with:
+  #   encfs <encrypted_foler> <decrypted_mount_point>
 
   # automount via pam_mount on login
-  security.pam.mount.enable = false;
+  security.pam.mount.enable = true;
   security.pam.mount.additionalSearchPaths = [ cryptor ];
   security.pam.mount.extraVolumes = [
-    ''<volume user="${user}" fstype="fuse" path="encfs#${encryptedDir}" mountpoint="${mountDir}" options="nonempty" />''
+    ''<volume user="${user}" fstype="fuse" path="encfs#${encryptedDir}" mountpoint="${mountDir}" options="nodev,nosuid" />''
   ];
 }
