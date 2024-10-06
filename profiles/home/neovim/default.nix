84a1ebcbe1a263c6e0b5bf1148152353346076a6
Yipeng Sun <syp@umd.edu>
2024-10-06 14:55:37 +0800

    Neovim: wip #20: more tweak to the debugging workflow.

--- profiles/home/neovim/default.nix
+++ profiles/home/neovim/default.nix
@@ -99,7 +99,12 @@ in
             endif
           '';
         }
-        direnv-vim
+        {
+          plugin = direnv-vim;
+          config = ''
+            let g:direnv_silent_load = 1
+          '';
+        }
         delimitMate
         lastchange
 
@@ -188,7 +193,6 @@ in
         }
 
         # debug
-        nvim-dap-virtual-text
         nvim-dap-ui
         {
           plugin = nvim-dap;
@@ -209,7 +213,6 @@ in
               }
 
               dapui.setup();
-              require("nvim-dap-virtual-text").setup()
 
               dap.listeners.before.attach.dapui_config = function()
                 dapui.open()
@@ -362,6 +365,30 @@ in
                 suggestions = { enable = false },
                 panel = { enable = false },
               }
+
+              -- lsp warning signs
+              local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
+              for type, icon in pairs(signs) do
+                local hl = "DiagnosticSign" .. type
+                vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+              end
+
+              -- show line diagnostics automatically in hover window
+              vim.o.updatetime = 200
+              vim.api.nvim_create_autocmd("CursorHold", {
+                buffer = bufnr,
+                callback = function()
+                  local opts = {
+                    focusable = false,
+                    close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+                    border = 'rounded',
+                    source = 'always',
+                    prefix = ' ',
+                    scope = 'cursor',
+                  }
+                  vim.diagnostic.open_float(nil, opts)
+                end
+              })
 
               -- auto completion
               local capabilities = require("cmp_nvim_lsp").default_capabilities()
