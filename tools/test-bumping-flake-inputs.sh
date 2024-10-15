86c06544fa1deb4ff90ebec3c98f557d6ad999b5
Yipeng Sun <syp@umd.edu>
2024-10-15 14:54:48 +0800

    Be more verbose in CI.

--- tools/test-bumping-flake-inputs.sh
+++ tools/test-bumping-flake-inputs.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
 
+echo "Updating nixpkgs input..."
+echo ""
 nix flake update \
     --override-input nixpkgs "github:nixos/nixpkgs/nixos-unstable"
 
+echo "Building hosts"
+echo ""
 for h in Henri Thomas; do
+    echo ""
+    echo "########"
+    echo "Building $h..."
+    echo "########"
+    echo ""
+
     nix build \
         ".#nixosConfigurations.$h.config.system.build.toplevel" \
         --override-input nixpkgs "github:nixos/nixpkgs/nixos-unstable"
+
+    echo ""
+    echo "########"
+    echo "$h built successfully."
+    echo "########"
+    echo ""
 done
