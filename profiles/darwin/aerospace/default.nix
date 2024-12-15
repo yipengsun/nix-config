16e05f5f41177fa6639fd8f00012ad8186a45ac9
Yipeng Sun <syp@umd.edu>
2024-12-15 13:57:29 +0800

    Aerospace: tweak config.

--- profiles/darwin/aerospace/default.nix
+++ profiles/darwin/aerospace/default.nix
@@ -1,3 +1,6 @@
+let
+  modkey = "cmd";
+in
 {
   services.aerospace = {
     enable = true;
@@ -9,99 +12,80 @@
         outer.top = 3;
         outer.bottom = 3;
 
-        inner.horizontal = 2;
-        inner.vertical = 2;
+        inner.horizontal = 3;
+        inner.vertical = 3;
       };
+      accordion-padding = 250;
+
+      default-root-container-layout = "tiles";
+
+      on-focused-monitor-changed = [ "move-mouse monitor-lazy-center" ];
+      #on-focus-changed = "move-mouse window-lazy-center";
 
       automatically-unhide-macos-hidden-apps = true;
 
       mode.main.binding = {
-        alt-slash = "layout tiles horizontal vertical";
-        alt-comma = "layout accordion horizontal vertical";
+        "${modkey}-slash" = "layout tiles horizontal vertical";
+        "${modkey}-backslash" = "layout accordion horizontal vertical";
 
-        alt-h = "focus left";
-        alt-j = "focus down";
-        alt-k = "focus up";
-        alt-l = "focus right";
+        "${modkey}-h" = "focus left";
+        "${modkey}-j" = "focus down";
+        "${modkey}-k" = "focus up";
+        "${modkey}-l" = "focus right";
 
-        alt-shift-h = "move left";
-        alt-shift-j = "move down";
-        alt-shift-k = "move up";
-        alt-shift-l = "move right";
+        "${modkey}-shift-h" = "move left";
+        "${modkey}-shift-j" = "move down";
+        "${modkey}-shift-k" = "move up";
+        "${modkey}-shift-l" = "move right";
 
-        alt-minus = "resize smart -50";
-        alt-equal = "resize smart +50";
+        "${modkey}-minus" = "resize smart -50";
+        "${modkey}-equal" = "resize smart +50";
 
-        alt-tab = "workspace-back-and-forth";
-        alt-shift-tab = "move-workspace-to-monitor --wrap-around next";
+        "alt-tab" = "workspace-back-and-forth";
+        "alt-shift-tab" = "move-workspace-to-monitor --wrap-around next";
+
+        "${modkey}-a" = "macos-native-fullscreen";
+        "${modkey}-f" = "close";
+        "${modkey}-d" = "macos-native-minimize";
+
+        "${modkey}-m" = "layout floating tiling";
+
+        "${modkey}-left" = "workspace --wrap-around prev";
+        "${modkey}-right" = "workspace --wrap-around next";
 
 
         # workspaces
-        alt-1 = "workspace 1";
-        alt-2 = "workspace 2";
-        alt-3 = "workspace 3";
-        alt-4 = "workspace 4";
-        alt-5 = "workspace 5";
-        alt-6 = "workspace 6";
-        alt-7 = "workspace 7";
-        alt-8 = "workspace 8";
-        alt-9 = "workspace 9";
-        alt-a = "workspace A";
-        alt-b = "workspace B";
-        alt-c = "workspace C";
-        alt-d = "workspace D";
-        alt-e = "workspace E";
-        alt-f = "workspace F";
-        alt-g = "workspace G";
-        alt-i = "workspace I";
-        alt-m = "workspace M";
-        alt-n = "workspace N";
-        alt-o = "workspace O";
-        alt-p = "workspace P";
-        alt-q = "workspace Q";
-        alt-r = "workspace R";
-        alt-s = "workspace S";
-        alt-t = "workspace T";
-        alt-u = "workspace U";
-        alt-v = "workspace V";
-        alt-x = "workspace X";
-        alt-y = "workspace Y";
-        alt-z = "workspace Z";
+        "${modkey}-1" = "workspace 1";
+        "${modkey}-2" = "workspace 2";
+        "${modkey}-3" = "workspace 3";
+        "${modkey}-4" = "workspace 4";
+        "${modkey}-5" = "workspace 5";
+        "${modkey}-6" = "workspace 6";
+        "${modkey}-7" = "workspace 7";
+        "${modkey}-8" = "workspace 8";
+        "${modkey}-9" = "workspace 9";
 
-        alt-shift-1 = "move-node-to-workspace 1";
-        alt-shift-2 = "move-node-to-workspace 2";
-        alt-shift-3 = "move-node-to-workspace 3";
-        alt-shift-4 = "move-node-to-workspace 4";
-        alt-shift-5 = "move-node-to-workspace 5";
-        alt-shift-6 = "move-node-to-workspace 6";
-        alt-shift-7 = "move-node-to-workspace 7";
-        alt-shift-8 = "move-node-to-workspace 8";
-        alt-shift-9 = "move-node-to-workspace 9";
-        alt-shift-a = "move-node-to-workspace A";
-        alt-shift-b = "move-node-to-workspace B";
-        alt-shift-c = "move-node-to-workspace C";
-        alt-shift-d = "move-node-to-workspace D";
-        alt-shift-e = "move-node-to-workspace E";
-        alt-shift-f = "move-node-to-workspace F";
-        alt-shift-g = "move-node-to-workspace G";
-        alt-shift-i = "move-node-to-workspace I";
-        alt-shift-m = "move-node-to-workspace M";
-        alt-shift-n = "move-node-to-workspace N";
-        alt-shift-o = "move-node-to-workspace O";
-        alt-shift-p = "move-node-to-workspace P";
-        alt-shift-q = "move-node-to-workspace Q";
-        alt-shift-r = "move-node-to-workspace R";
-        alt-shift-s = "move-node-to-workspace S";
-        alt-shift-t = "move-node-to-workspace T";
-        alt-shift-u = "move-node-to-workspace U";
-        alt-shift-v = "move-node-to-workspace V";
-        alt-shift-x = "move-node-to-workspace X";
-        alt-shift-y = "move-node-to-workspace Y";
-        alt-shift-z = "move-node-to-workspace Z";
+        "${modkey}-i" = "workspace I";
+        "${modkey}-o" = "workspace O";
+        "${modkey}-p" = "workspace P";
+
+        "${modkey}-shift-1" = "move-node-to-workspace 1";
+        "${modkey}-shift-2" = "move-node-to-workspace 2";
+        "${modkey}-shift-3" = "move-node-to-workspace 3";
+        "${modkey}-shift-4" = "move-node-to-workspace 4";
+        "${modkey}-shift-5" = "move-node-to-workspace 5";
+        "${modkey}-shift-6" = "move-node-to-workspace 6";
+        "${modkey}-shift-7" = "move-node-to-workspace 7";
+        "${modkey}-shift-8" = "move-node-to-workspace 8";
+        "${modkey}-shift-9" = "move-node-to-workspace 9";
+
+        "${modkey}-shift-i" = "move-node-to-workspace I";
+        "${modkey}-shift-o" = "move-node-to-workspace O";
+        "${modkey}-shift-p" = "move-node-to-workspace P";
 
 
         # another mode
-        alt-shift-semicolon = "mode service";
+        "${modkey}-shift-semicolon" = "mode service";
       };
 
       mode.service.binding = {
@@ -111,10 +95,27 @@
         f = [ "layout floating tiling" "mode main" ]; # toggle between floating and tiling layout
         backspace = [ "close-all-windows-but-current" "mode main" ];
 
-        alt-shift-h = [ "join-with left" "mode main" ];
-        alt-shift-j = [ "join-with down" "mode main" ];
-        alt-shift-k = [ "join-with up" "mode main" ];
-        alt-shift-l = [ "join-with right" "mode main" ];
+        "${modkey}-shift-h" = [ "join-with left" "mode main" ];
+        "${modkey}-shift-j" = [ "join-with down" "mode main" ];
+        "${modkey}-shift-k" = [ "join-with up" "mode main" ];
+        "${modkey}-shift-l" = [ "join-with right" "mode main" ];
+      };
+
+      workspace-to-monitor-force-assignment = {
+        "1" = "built-in";
+        "2" = "built-in";
+        "3" = "built-in";
+        "4" = "built-in";
+        "5" = "built-in";
+
+        "6" = [ "secondary" "built-in" ];
+        "7" = [ "secondary" "built-in" ];
+        "8" = [ "secondary" "built-in" ];
+        "9" = [ "secondary" "built-in" ];
+
+        "I" = [ "secondary" "built-in" ];
+        "O" = [ "secondary" "built-in" ];
+        "P" = [ "secondary" "built-in" ];
       };
     };
   };
