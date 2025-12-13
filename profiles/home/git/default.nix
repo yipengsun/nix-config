8ded0952ccc1a639f5252a924e2f710a8d6bdc18
Yipeng Sun <syp@umd.edu>
2025-12-13 22:42:58 +0800

    git: migrate config.

--- profiles/home/git/default.nix
+++ profiles/home/git/default.nix
@@ -1,4 +1,39 @@
 { pkgs, ... }:
+let
+  aliases = {
+    a = "add";
+    co = "checkout";
+    cob = "checkout -b";
+    f = "fetch -p";
+    c = "commit";
+    p = "push";
+    pl = "pull";
+    ba = "branch -a";
+    bd = "branch -d";
+    bD = "branch -D";
+    d = "diff";
+    dc = "diff --cached";
+    ds = "diff --staged";
+    r = "restore";
+    rs = "restore --staged";
+    st = "status -sb";
+
+    # reset
+    soft = "reset --soft";
+    hard = "reset --hard";
+    s1ft = "soft HEAD~1";
+    h1rd = "hard HEAD~1";
+
+    # logging
+    lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+    plog = "log --graph --pretty='format:%C(red)%d%C(reset) %C(yellow)%h%C(reset) %ar %C(green)%aN%C(reset) %s'";
+    tlog = "log --stat --since='1 Day Ago' --graph --pretty=oneline --abbrev-commit --date=relative";
+    rank = "shortlog -sn --no-merges";
+
+    # delete merged branches
+    bdm = "!git branch --merged | grep -v '*' | xargs -n 1 git branch -d";
+  };
+in
 {
   home.packages = [ pkgs.tig ];
 
@@ -25,7 +60,7 @@
     enable = true;
     lfs.enable = true;
 
-    extraConfig = {
+    settings = {
       pull.rebase = false;
       core.editor = "nvim";
       core.pager = "less";
@@ -37,40 +72,8 @@
 
       features.manyFiles = true;
       init.defaultBranch = "master";
-    };
 
-    aliases = {
-      a = "add";
-      co = "checkout";
-      cob = "checkout -b";
-      f = "fetch -p";
-      c = "commit";
-      p = "push";
-      pl = "pull";
-      ba = "branch -a";
-      bd = "branch -d";
-      bD = "branch -D";
-      d = "diff";
-      dc = "diff --cached";
-      ds = "diff --staged";
-      r = "restore";
-      rs = "restore --staged";
-      st = "status -sb";
-
-      # reset
-      soft = "reset --soft";
-      hard = "reset --hard";
-      s1ft = "soft HEAD~1";
-      h1rd = "hard HEAD~1";
-
-      # logging
-      lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
-      plog = "log --graph --pretty='format:%C(red)%d%C(reset) %C(yellow)%h%C(reset) %ar %C(green)%aN%C(reset) %s'";
-      tlog = "log --stat --since='1 Day Ago' --graph --pretty=oneline --abbrev-commit --date=relative";
-      rank = "shortlog -sn --no-merges";
-
-      # delete merged branches
-      bdm = "!git branch --merged | grep -v '*' | xargs -n 1 git branch -d";
+      alias = aliases;
     };
   };
 }
