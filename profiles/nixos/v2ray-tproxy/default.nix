607228624d11d43efd0c527f93160885f110fc06
Yipeng Sun <syp@umd.edu>
2026-08-16 15:29:19 +0800

    Deduplicate V2Ray firewall policy.

--- profiles/nixos/v2ray-tproxy/default.nix
+++ profiles/nixos/v2ray-tproxy/default.nix
@@ -2,11 +2,67 @@
   self,
   pkgs,
   config,
+  lib,
   ...
 }:
 let
   ip = "${pkgs.iproute2}/bin/ip";
   iptables = "${pkgs.iptables}/bin/iptables";
+
+  policy = {
+    mark = "1";
+    bypassMark = "0xff";
+    routeTable = "100";
+    localRoute = "0.0.0.0/0";
+
+    tproxyAddress = "127.0.0.1";
+    tproxyBypassCidr = "127.0.0.1/32";
+    tproxyPort = "5280";
+
+    dnsPort = "53";
+    udpDnsProxyCidrs = [
+      "10.0.0.0/8"
+      "169.254.0.0/16"
+      "172.16.0.0/12"
+      "192.168.0.0/16"
+      "224.0.0.0/4"
+      "240.0.0.0/4"
+    ];
+    alwaysBypassCidr = "255.255.255.255/32";
+  };
+
+  proxyProtocols = [
+    "udp"
+    "tcp"
+  ];
+  cidrReturnProtocols = [
+    "tcp"
+    "udp"
+  ];
+
+  mkProtocolRules = protocols: mkRule: lib.concatMapStringsSep "\n" mkRule protocols;
+
+  tproxyRules = mkProtocolRules proxyProtocols (
+    protocol:
+    "${iptables} -t mangle -A V2RAY -p ${protocol} -j TPROXY"
+    + " --on-ip ${policy.tproxyAddress}"
+    + " --on-port ${policy.tproxyPort}"
+    + " --tproxy-mark ${policy.mark}"
+  );
+
+  outputCidrReturnRules = lib.concatMapStringsSep "\n" (
+    cidr:
+    mkProtocolRules cidrReturnProtocols (
+      protocol:
+      "${iptables} -t mangle -A V2RAY_MASK -d ${cidr} -p ${protocol}${
+        lib.optionalString (protocol == "udp") " ! --dport ${policy.dnsPort}"
+      } -j RETURN"
+    )
+  ) policy.udpDnsProxyCidrs;
+
+  outputMarkRules = mkProtocolRules proxyProtocols (
+    protocol: "${iptables} -t mangle -A V2RAY_MASK -p ${protocol} -j MARK --set-mark ${policy.mark}"
+  );
 
   cleanupLegacyFirewall = ''
     while ${iptables} -t mangle -C PREROUTING -p tcp -m socket -j DIVERT 2>/dev/null; do
@@ -32,8 +88,8 @@ let
       ${iptables} -t mangle -X "$chain" 2>/dev/null || true
     done
 
-    while ${ip} rule del fwmark 1 table 100 2>/dev/null; do :; done
-    ${ip} route del local 0.0.0.0/0 dev lo table 100 2>/dev/null || true
+    while ${ip} rule del fwmark ${policy.mark} table ${policy.routeTable} 2>/dev/null; do :; done
+    ${ip} route del local ${policy.localRoute} dev lo table ${policy.routeTable} 2>/dev/null || true
   '';
 in
 {
@@ -58,11 +114,9 @@ in
     LimitNOFILE = 1000000;
   };
 
-  # enable forwarding for both IPv4 & IPv6
-  #boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
-  #boot.kernel.sysctl."net.ipv6.conf.all.forwarding" = 1;
+  # This profile does not enable IP forwarding; its transparent-proxy policy is IPv4-only.
 
-  # setup iptables mangling rules
+  # Set up IPv4 iptables mangle and policy-routing rules.
   #   main guide: https://guide.v2fly.org/app/tproxy.html
   #   related doc: https://www.kernel.org/doc/Documentation/networking/tproxy.rst
   networking.firewall.extraCommands = ''
@@ -71,36 +125,23 @@ in
 
     ${iptables} -t mangle -N V2RAY_DIVERT
     ${iptables} -t mangle -I PREROUTING -p tcp -m socket -j V2RAY_DIVERT
-    ${iptables} -t mangle -A V2RAY_DIVERT -j MARK --set-mark 1
+    ${iptables} -t mangle -A V2RAY_DIVERT -j MARK --set-mark ${policy.mark}
     ${iptables} -t mangle -A V2RAY_DIVERT -j ACCEPT
 
-    ${ip} rule add fwmark 1 table 100
-    ${ip} route add local 0.0.0.0/0 dev lo table 100
+    ${ip} rule add fwmark ${policy.mark} table ${policy.routeTable}
+    ${ip} route add local ${policy.localRoute} dev lo table ${policy.routeTable}
 
     ${iptables} -t mangle -N V2RAY
-    ${iptables} -t mangle -A V2RAY -j RETURN -m mark --mark 0xff
-    ${iptables} -t mangle -A V2RAY -d 127.0.0.1/32 -j RETURN
-    ${iptables} -t mangle -A V2RAY -p udp -j TPROXY --on-ip 127.0.0.1 --on-port 5280 --tproxy-mark 1
-    ${iptables} -t mangle -A V2RAY -p tcp -j TPROXY --on-ip 127.0.0.1 --on-port 5280 --tproxy-mark 1
+    ${iptables} -t mangle -A V2RAY -j RETURN -m mark --mark ${policy.bypassMark}
+    ${iptables} -t mangle -A V2RAY -d ${policy.tproxyBypassCidr} -j RETURN
+    ${tproxyRules}
     ${iptables} -t mangle -A PREROUTING -j V2RAY
 
     ${iptables} -t mangle -N V2RAY_MASK
-    ${iptables} -t mangle -A V2RAY_MASK -j RETURN -m mark --mark 0xff
-    ${iptables} -t mangle -A V2RAY_MASK -d 10.0.0.0/8 -p tcp -j RETURN
-    ${iptables} -t mangle -A V2RAY_MASK -d 10.0.0.0/8 -p udp ! --dport 53 -j RETURN
-    ${iptables} -t mangle -A V2RAY_MASK -d 169.254.0.0/16 -p tcp -j RETURN
-    ${iptables} -t mangle -A V2RAY_MASK -d 169.254.0.0/16 -p udp ! --dport 53 -j RETURN
-    ${iptables} -t mangle -A V2RAY_MASK -d 172.16.0.0/12 -p tcp -j RETURN
-    ${iptables} -t mangle -A V2RAY_MASK -d 172.16.0.0/12 -p udp ! --dport 53 -j RETURN
-    ${iptables} -t mangle -A V2RAY_MASK -d 192.168.0.0/16 -p tcp -j RETURN
-    ${iptables} -t mangle -A V2RAY_MASK -d 192.168.0.0/16 -p udp ! --dport 53 -j RETURN
-    ${iptables} -t mangle -A V2RAY_MASK -d 224.0.0.0/4 -p tcp -j RETURN
-    ${iptables} -t mangle -A V2RAY_MASK -d 224.0.0.0/4 -p udp ! --dport 53 -j RETURN
-    ${iptables} -t mangle -A V2RAY_MASK -d 240.0.0.0/4 -p tcp -j RETURN
-    ${iptables} -t mangle -A V2RAY_MASK -d 240.0.0.0/4 -p udp ! --dport 53 -j RETURN
-    ${iptables} -t mangle -A V2RAY_MASK -d 255.255.255.255/32 -j RETURN
-    ${iptables} -t mangle -A V2RAY_MASK -p udp -j MARK --set-mark 1
-    ${iptables} -t mangle -A V2RAY_MASK -p tcp -j MARK --set-mark 1
+    ${iptables} -t mangle -A V2RAY_MASK -j RETURN -m mark --mark ${policy.bypassMark}
+    ${outputCidrReturnRules}
+    ${iptables} -t mangle -A V2RAY_MASK -d ${policy.alwaysBypassCidr} -j RETURN
+    ${outputMarkRules}
     ${iptables} -t mangle -A OUTPUT -j V2RAY_MASK
   '';
   networking.firewall.extraStopCommands = cleanupFirewall;
