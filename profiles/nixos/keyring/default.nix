d721df24fce51aebbef3467ce3bdd36950de76c6
Yipeng Sun <syp@umd.edu>
2026-09-15 23:55:54 +0800

    Use explicit GCR 3 with a fallback for current pins

--- profiles/nixos/keyring/default.nix
+++ profiles/nixos/keyring/default.nix
@@ -2,6 +2,6 @@
 {
   services.gnome.gnome-keyring.enable = true;
 
-  services.dbus.packages = [ pkgs.gcr ]; # pin entry from GNOME
+  services.dbus.packages = [ (pkgs.gcr_3 or pkgs.gcr) ]; # FIXME: Drop the gcr fallback once the nixpkgs pin provides gcr_3.
   # ^this option should be in sync with that in "passwd-mgr"
 }
